7f88aaa48c3232df9fb1ece03a457ab1284f95cd
Matthias Jordan <matthias.k.jordan@gmail.com>
2016-09-13 15:39:45 -0700

    Minify'in all the things

--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -1,36 +1,18 @@
-// mobile menu drop down
-
-$(document).ready(function ($) {
-
-  var $toggle = $('.nav-toggle');
-  var $menu = $('.nav-menu');
-
-  $toggle.click(function() {
-    $(this).toggleClass('is-active');
-    $menu.toggleClass('is-active');
-  });
-});
-
-// scroll on click
-
-
-$('a[href^="#"]').on('click', function(event) {
-    var target = $(this.getAttribute('href'));
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 2000);
-    }
-});
-
-
-//macy
-
-Macy.init({
-    container: '.gallery',
-    trueOrder: false,
-    waitForImages: false,
+$(document).ready(function(e) {
+    var o = e(".nav-toggle"),
+        t = e(".nav-menu");
+    o.click(function() {
+        e(this).toggleClass("is-active"), t.toggleClass("is-active")
+    })
+}), $('a[href^="#"]').on("click", function(e) {
+    var o = $(this.getAttribute("href"));
+    o.length && (e.preventDefault(), $("html, body").stop().animate({
+        scrollTop: o.offset().top
+    }, 2e3))
+}), Macy.init({
+    container: ".gallery",
+    trueOrder: !1,
+    waitForImages: !1,
     margin: 24,
     columns: 5,
     breakAt: {
@@ -39,31 +21,20 @@ Macy.init({
         520: 2,
         400: 1
     }
-});
-
-
-// swipebox
-
-$('.swipebox').swipebox( {
-		useCSS : true, // false will force the use of jQuery for animations
-		useSVG : true, // false to force the use of png for buttons
-		initialIndexOnArray : 0, // which image index to init when a array is passed
-		hideCloseButtonOnMobile : false, // true will hide the close button on mobile devices
-		removeBarsOnMobile : true, // false will show top bar on mobile devices
-		hideBarsDelay : 3000, // delay before hiding bars on desktop
-		videoMaxWidth : 1140, // videos max width
-		beforeOpen: function() {}, // called before opening
-		afterOpen: null, // called after opening
-		afterClose: function() {}, // called after closing
-		loopAtEnd: false // true will return to the first image after the last image is reached
-	} );
-
-// cookiebar
-$.cookieBar = function( options ) {
-$('.hero-head').prepend('<section class="hero is-danger is-small"><div style="font-size: 12px; margin: 5px 0; padding: 0px 75px; line-height: 15px;" class="cookie-message"><p>Actively seeking <em><b>product management</b></em> contracts and full time positions. <a style="color:#fff" href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#109;&#101;&#64;&#105;&#97;&#109;&#109;&#97;&#116;&#116;&#104;&#105;&#97;&#115;&#46;&#99;&#111;&#109;"><u>Get in touch</u></a> or say "hello" to <a style="color:#fff" href="/bot"><u>MatthiasBot!</u></a><br><br><a style="color:#fff;" class="my-close-button" href><u>CLOSE</u></a></p></div></section>');
-$('.cookie-message').cookieBar({ closeButton : '.my-close-button' });
-};
-
-$.cookieBar();
-
-$("img.lazy").lazyload();
+}), $(".swipebox").swipebox({
+    useCSS: !0,
+    useSVG: !0,
+    initialIndexOnArray: 0,
+    hideCloseButtonOnMobile: !1,
+    removeBarsOnMobile: !0,
+    hideBarsDelay: 3e3,
+    videoMaxWidth: 1140,
+    beforeOpen: function() {},
+    afterOpen: null,
+    afterClose: function() {},
+    loopAtEnd: !1
+}), $.cookieBar = function(e) {
+    $(".hero-head").prepend('<section class="hero is-danger is-small"><div style="font-size: 12px; margin: 5px 0; padding: 0px 75px; line-height: 15px;" class="cookie-message"><p>Actively seeking <em><b>product management</b></em> contracts and full time positions. <a style="color:#fff" href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#109;&#101;&#64;&#105;&#97;&#109;&#109;&#97;&#116;&#116;&#104;&#105;&#97;&#115;&#46;&#99;&#111;&#109;"><u>Get in touch</u></a> or say "hello" to <a style="color:#fff" href="/bot"><u>MatthiasBot!</u></a><br><br><a style="color:#fff;" class="my-close-button" href><u>CLOSE</u></a></p></div></section>'), $(".cookie-message").cookieBar({
+        closeButton: ".my-close-button"
+    })
+}, $.cookieBar(), $("img.lazy").lazyload();
